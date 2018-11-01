07cafa43ca77e87828343e48a87c7e552e86897a
Paul Cook <1893351+Simcon@users.noreply.github.com>
2018-11-01 08:12:58 +0000

    Added more notes for Docker

--- boxstarter_for_dev_vm.ps1
+++ boxstarter_for_dev_vm.ps1
@@ -37,7 +37,7 @@ $Packages = 'firefox',`
             'visualstudio2017-workload-netcoretools',`
             'winrar'
             
-# Before installing Docker you must run: Set-VMProcessor -VMName "<Your Virtual Machine Name>" -ExposeVirtualizationExtensions $true
+# Before installing Docker you must run the following PowerShell commandlet on the host before powering on the VM: Set-VMProcessor -VMName "<Your Virtual Machine Name>" -ExposeVirtualizationExtensions $true
 # 'docker-for-windows',`            
 
 #--- Install Packages ---
